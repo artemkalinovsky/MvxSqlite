57aee0d1e35873c187cf7999fc46e1839810ef2d
Artem Kalinovsky <artem.kalinovsky@gmail.com>
2016-04-15 01:39:04 +0300

    Updated SqlitePluginBootstrap for Android

--- Droid/Bootstrap/SqlitePluginBootstrap.cs
+++ Droid/Bootstrap/SqlitePluginBootstrap.cs
@@ -2,7 +2,7 @@ using MvvmCross.Platform.Plugins;
 
 namespace MvxSqlite.Droid.Bootstrap
 {
-    public class SqlitePluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Sqlite.PluginLoader>
-		{}
+	public class SqlitePluginBootstrap
+		: MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.Sqlite.PluginLoader, MvvmCross.Plugins.Sqlite.Droid.Plugin>
+	{}
 }
